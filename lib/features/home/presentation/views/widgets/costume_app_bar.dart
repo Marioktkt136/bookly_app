c1424f03d8bdcf287c39ecddbb7e9238af2a7af3
Mario Naiem Bedrous <62382441+Marioktkt136@users.noreply.github.com>
2024-07-18 16:36:04 +0300

    add customApp to home view

---
+++ lib/features/home/presentation/views/widgets/costume_app_bar.dart
@@ -0,0 +1,29 @@
+import 'package:bookly_app/assets_data.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 28, vertical: 28.0),
+      child: Row(
+        children: [
+          Image.asset(
+            AssetsData.logo,
+            height: 16,
+          ),
+          const Spacer(),
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                FontAwesomeIcons.magnifyingGlass,
+                size: 24,
+              )),
+        ],
+      ),
+    );
+  }
+}
