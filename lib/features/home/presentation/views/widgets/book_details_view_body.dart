131eeb6c7aa53ba2e2cb50e80d579e75989e61fa
Mario Naiem Bedrous <62382441+Marioktkt136@users.noreply.github.com>
2024-10-27 16:22:12 +0300

    custom_book_image

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_book_details_view_app_bar.dart';
@@ -7,11 +8,16 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    var width = MediaQuery.of(context).size.width;
+    return Column(
       children: [
-        Padding(
+        const Padding(
           padding: EdgeInsets.symmetric(horizontal: 30.0),
           child: CustomBookDetailsViewAppBar(),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: width * 0.23),
+          child: const CustomBookImage(),
         ),
       ],
     );
